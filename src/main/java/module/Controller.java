04e1da6dd49f6152829c92b4162b9a57fa24a4d4
Frank <javier.francisco01.09@gmail.com>
2018-03-01 20:46:59 -0600

    controller de main con datos, aun no tiene funciones

--- src/main/java/module/Controller.java
+++ src/main/java/module/Controller.java
@@ -1,4 +1,26 @@
 package module;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class Controller {
+    @FXML
+    TextField userLoginText;
+    @FXML
+    TextField userLoginPass;
+    @FXML
+    Button loginBtn;
+    @FXML
+    ComboBox<String> comboType;
+    @FXML
+    Label loginLabel;
+    @FXML
+    Label labelUsuario;
+    @FXML
+    Label labelContrasena;
+    @FXML
+    private void login_Btn(){
+
+    }
+
 }
