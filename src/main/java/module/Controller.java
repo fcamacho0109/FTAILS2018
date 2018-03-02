893c60d8273e0794ff404910f0336664f1c8e27e
Frank <javier.francisco01.09@gmail.com>
2018-03-01 20:53:09 -0600

    alert

--- src/main/java/module/Controller.java
+++ src/main/java/module/Controller.java
@@ -20,7 +20,14 @@ public class Controller {
     Label labelContrasena;
     @FXML
     private void login_Btn(){
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Information Dialog");
+        alert.setHeaderText(null);
+        alert.setContentText("Aun no se definen acciones para el boton!" +
+                "\nEditar metodo login_Btn() para agregar login \n" +
+                "con user role y password de la BD");
 
+        alert.showAndWait();
     }
 
 }
